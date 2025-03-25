1597f7e4067d1a5b7fbbe355becc405e018e880f
fei man <fman@student.unimelb.edu.au>
2025-03-25 16:32:12 +1100

    add student class

--- app/src/main/java/com/unimelb/swen30006/workshops/Student.java
+++ app/src/main/java/com/unimelb/swen30006/workshops/Student.java
@@ -1,4 +1,17 @@
 package com.unimelb.swen30006.workshops;
 
 public class Student {
+    public String studentId;
+    public String email;
+
+    public void sendEmailMsg(String studentid, String email) {
+
+        this.email = email;
+        this.studentId = studentId;
+    }
+
+    public float totalGrade(){
+        return 0;
+    }
 }
+
